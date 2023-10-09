5c9a50ecda8ed9c1a97eaffd9d51ac26741d6e9d
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-10-09 00:33:51 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "This is a test game test game";
+const clickerText = "Click ME 😎";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const clicker = document.getElementById("clicker");
+clicker!.innerHTML = clickerText;
+app.append(clicker!);
