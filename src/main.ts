89f565924f194d1554736ebcb85a702e18ad6dc8
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-10-11 21:15:43 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,39 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName: string = "This is a test game test game";
 const clickerText: string = "Click ME 😎";
-const upgradeText: string = "10 😎 --> 0.1 Auto 😎";
+const upgradeAText: string = "10 😎 --> 0.1 Auto 😎";
+const upgradeBText: string = "100 😎 --> 2 Auto 😎";
+const upgradeCText: string = "1000 😎 --> 50 Auto 😎";
 
 let counter: number = 0;
 let timestamp: number = 0;
-let numUpgraded = 0;
+let numA: number = 0;
+let numB: number = 0;
+let numC: number = 0;
 
 function increaseCount(n: number) {
   counter += n;
-  countText!.innerHTML = `😎 ${counter} 😎`;
+  countText!.innerHTML = `😎 ${counter.toFixed(2)} 😎`;
+  rateText!.innerHTML = `${calculateGrowthRate().toFixed(2)} 😎/sec`;
 }
 
 function automaticIncrease() {
   const timePassed = performance.now() - timestamp;
-  increaseCount(numUpgraded * (timePassed / 1000));
+  increaseCount(calculateGrowthRate() * (timePassed / 1000));
   timestamp = performance.now();
   window.requestAnimationFrame(automaticIncrease);
+
+  if (counter >= 100 && (upgraderB! as HTMLSelectElement).disabled != false) {
+    (upgraderB! as HTMLSelectElement).disabled = false;
+  }
+
+  if (counter >= 1000 && (upgraderC! as HTMLSelectElement).disabled != false) {
+    (upgraderC! as HTMLSelectElement).disabled = false;
+  }
+}
+
+function calculateGrowthRate(): number {
+  return 0.1 * numA + 2 * numB + 50 * numC;
 }
 
 document.title = gameName;
@@ -32,26 +49,54 @@ const clicker = document.getElementById("clicker");
 clicker!.innerHTML = clickerText;
 app.append(clicker!);
 
+app.append(document.createElement("br"));
+
+const upgraderA = document.getElementById("upgradeA");
+upgraderA!.innerHTML = upgradeAText;
+app.append(upgraderA!);
+
+const upgraderB = document.getElementById("upgradeB");
+upgraderB!.innerHTML = upgradeBText;
+app.append(upgraderB!);
+
+const upgraderC = document.getElementById("upgradeC");
+upgraderC!.innerHTML = upgradeCText;
+app.append(upgraderC!);
+
 const countText = document.getElementById("count");
-countText!.innerHTML = `😎 ${counter} 😎`;
+countText!.innerHTML = `😎 0.00 😎`;
 app.append(countText!);
 
-const upgrader = document.getElementById("upgrade");
-upgrader!.innerHTML = upgradeText;
-app.append(upgrader!);
+const rateText = document.getElementById("growthRate");
+rateText!.innerHTML = `0.00 😎/sec`;
+app.append(rateText!);
 
 clicker?.addEventListener("click", () => {
-  increaseCount(1);
+  increaseCount(100);
   if (counter >= 10) {
-    (upgrader! as HTMLSelectElement).disabled = false;
+    (upgraderA! as HTMLSelectElement).disabled = false;
   }
 });
 
-upgrader?.addEventListener("click", () => {
+upgraderA?.addEventListener("click", () => {
   if (counter >= 10) {
     counter -= 10;
-    numUpgraded += 1;
+    numA += 1;
     timestamp = performance.now();
     window.requestAnimationFrame(automaticIncrease);
+  }
+});
+
+upgraderB?.addEventListener("click", () => {
+  if (counter >= 100) {
+    counter -= 100;
+    numB += 1;
+  }
+});
+
+upgraderC?.addEventListener("click", () => {
+  if (counter >= 1000) {
+    counter -= 1000;
+    numC += 1;
   }
 });
